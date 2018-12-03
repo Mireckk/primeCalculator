5ae9f0502a29ea7f7191c6c02cb51ac79c35f8c0
Mireckk <mirekmic@gmail.com>
2018-12-03 12:52:13 +0100

    Update primeCalculator.js

--- primeCalculator.js
+++ primeCalculator.js
@@ -1,18 +1,18 @@
 const arr = [];
 const size = 1000;
-let i=1;
+let k=1;
 
 for(let i=2; i<size; i++) {
 	arr.push(i);
 }
 
-while(i<arr.length){
+while(k<arr.length){
 	for(let j=arr[i]*2; j<size; j=j+arr[i]) {
   	if(arr.indexOf(j)>0) {
 	  	arr.splice(arr.indexOf(j), 1);
     }
   }
-  i++;
+  k++;
 }
 
 
